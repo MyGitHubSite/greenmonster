024b20fd8f4986a58932f78b09b4b80de0f88ef5
Alan Wells <alan@adub.net>
2017-10-19 20:36:11 -0700

    change console output

--- manage.py
+++ manage.py
@@ -172,24 +172,25 @@ def drive(cfg, model_path=None, use_joystick=False):
         if (cfg.DEBUG):
             string = "Mode: " + mode
 
-            if mode == 'user':
-                string += " | Angle: " + str(round(user_angle, 2))
-                string += " | Throttle: " + str(round(user_throttle, 2))
+            if user_throttle > 0: 
+                if mode == 'user':
+                    string += " | Angle: " + str(round(user_angle, 2))
+                    string += " | Throttle: " + str(round(user_throttle, 2))
 
-            elif mode == 'local_angle':
-                string += " | Angle: " + str(round(pilot_angle, 2))
-                string += " | Throttle: " + str(round(user_throttle, 2))
-            
-            else:
-                string += " | Angle: " + str(round(pilot_angle, 2))
-                string += " | Throttle: " + str(round(pilot_throttle, 2))
+                elif mode == 'local_angle':
+                    string += " | Angle: " + str(round(pilot_angle, 2))
+                    string += " | Throttle: " + str(round(user_throttle, 2))
+                
+                else:
+                    string += " | Angle: " + str(round(pilot_angle, 2))
+                    string += " | Throttle: " + str(round(pilot_throttle, 2))
 
-            string += " | Distance: " + str(round(distance, 4)) + " m"
-            string += " | Velocity: " + str(round(velocity, 4)) + " m/s"
+                string += "\n | Distance: " + str(round(distance, 4)) + " m"
+                string += " | Velocity: " + str(round(velocity, 4)) + " m/s"
 
-            string += " | Target T: " + str(round(target_throttle, 2))
-            string += " | Measured T: " + str(round(measured_throttle, 2))
-            string += " | PID T: " + str(round(pid_throttle, 4))
+                string += "\n | Target T: " + str(round(target_throttle, 2))
+                string += " | Measured T: " + str(round(measured_throttle, 2))
+                string += " | PID T: " + str(round(pid_throttle, 4))
 
             print(string, end='\r')
             sys.stdout.flush()
